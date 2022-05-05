a94a4671b61eefdb21b6505467cb045239ad4876
Juliana <juaiasi@gmail.com>
2022-05-05 14:27:08 -0300

    lendo um arquivo

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import chalk from 'chalk'
+import chalk from 'chalk' //biblioteca que foi instalada com npm install
+import fs from 'fs' //biblioteca nativa para acesso aos arquivos do sistema (files)
 
-console.table(chalk.red("oi"))
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//                                             //callback, o que mostra se der erro e se der sucess. como quero ignorar o primeiro parâmetro, coloco underscore
+//     fs.readFile(caminhoDoArquivo, encoding, (_,texto)=>{
+//         console.log(chalk.green(texto)) //pinta de verde
+//     })
+// }
 
-const texto = 'Texto'
+function tratarErro(erro){
+    // lança novo erro          //código do erro, mas não precisa do .code (mas abrevia)
+    throw new Error(chalk.red(erro.code,'não há arquivo no caminho'))
+}
+
+function pegaArquivo(caminhoDoArquivo){
+    const encoding = 'utf-8'
+                                            //callback, o que mostra se der erro e se der sucess.
+    fs.readFile(caminhoDoArquivo, encoding, (erro,texto)=>{
+        if(erro){
+            tratarErro(erro);
+        }
+        console.log(chalk.green(texto)) //pinta de verde
+    })
+}
+
+
+pegaArquivo('./arquivos/texto1.md')
