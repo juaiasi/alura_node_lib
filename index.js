99f8e463f6f3fc8b4390ad4a1f8821023ddff49b
Juliana <juaiasi@gmail.com>
2022-05-06 09:24:43 -0300

    uutilizando async para abrir o arquivo e ler

--- index.js
+++ index.js
@@ -14,16 +14,41 @@ function tratarErro(erro){
     throw new Error(chalk.red(erro.code,'não há arquivo no caminho'))
 }
 
-function pegaArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8'
-                                            //callback, o que mostra se der erro e se der sucess.
-    fs.readFile(caminhoDoArquivo, encoding, (erro,texto)=>{
-        if(erro){
-            tratarErro(erro);
-        }
-        console.log(chalk.green(texto)) //pinta de verde
-    })
+// construindo a função de froma assincrona usando then
+async function pegaArquivo(caminhoDoArquivo){ //async
+    const encoding = 'utf-8';
+    try{
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding) //await
+        console.table(chalk.green(texto))
+    } 
+    catch(erro){
+        tratarErro(erro)
+    }
+    
 }
+
+// // construindo a função de froma assincrona usando then
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     //asincrono: espera processar para fazer, enquanto isso faz outras coisas
+//     //os que estão com pondo podem ser tudo na mesma linha, mas devem estar na ordem
+//     //fs já permite promises
+//     fs.promises
+//     .readFile()
+//     .then((texto) => console.table(chalk.blue(texto)))
+//     .catch((erro) => tratarErro()) //caso dê erro
+// }
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8'
+//                                             //callback, o que mostra se der erro e se der sucess.
+//     fs.readFile(caminhoDoArquivo, encoding, (erro,texto)=>{
+//         if(erro){
+//             tratarErro(erro);
+//         }
+//         console.log(chalk.green(texto)) //pinta de verde
+//     })
+// }
 
 
 pegaArquivo('./arquivos/texto1.md')
